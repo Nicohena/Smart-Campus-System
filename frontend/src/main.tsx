94b8e9d02ecd263bf58115832106e9987d223c58
Nicohena <nicohena19@gmail.com>
2026-04-08 10:05:11 +0300

    feat: add Toaster component for enhanced user notifications

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
 import App from './App'
 
@@ -8,6 +9,17 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3500,
+          style: {
+            background: '#111827',
+            color: '#f3f4f6',
+            border: '1px solid rgba(255,255,255,0.08)',
+          },
+        }}
+      />
     </BrowserRouter>
   </StrictMode>,
 )
